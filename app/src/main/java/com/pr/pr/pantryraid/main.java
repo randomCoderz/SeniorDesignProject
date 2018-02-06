2621703388f087031256f728c72bbb0c3b5f96f5
Am <am260700@gmail.com>
2018-02-05 22:44:50 -0800

    Fixed code
    Code clean up

--- app/src/main/java/com/pr/pr/pantryraid/main.java
+++ app/src/main/java/com/pr/pr/pantryraid/main.java
@@ -1,21 +1,20 @@
 package com.pr.pr.pantryraid;
 
-import android.os.AsyncTask;
+//Android Tools
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-//Unirest, Spoonacular Imports
+//Unirest, Spoonacular Imports, JSON
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.exceptions.UnirestException;
+
+import org.json.JSONObject;
 
 //FireBase Imports
 import com.google.firebase.database.DatabaseReference;
@@ -23,19 +22,32 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+
 import android.util.Log;
 
 public class main extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    @Override
-    protected void onCreate(Bundle savedInstanceState)  {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    private static final String KEY = "5EiGHCnHhQmshhJo3ecXqsynDWfip1v46Iwjsn83KwprhwkP1v"; //Place Your Key Here!
+    private static final String URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?diet=vegetarian&excludeIngredients" +
+            "=coconut&instructionsRequired=true&intolerances=egg%2C+gluten&limitLicense=false&number=10&offset=0&query=burger&type=main+course";
 
-        networkConnection p = new networkConnection();
-        p.start();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            networkConnection p = new networkConnection();
+            p.start();
+            p.join();
+            HttpResponse<JsonNode> response = p.getResponse();
+            JSONObject object = response.getBody().getObject();
+            TextView text = (TextView) findViewById(R.id.text);
+            text.setText(object.toString(2));
+        } catch (Exception e) {
+            System.err.println("Network Connection Error.");
+        }
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
@@ -60,21 +72,28 @@ public class main extends AppCompatActivity {
     }
 
     class networkConnection extends Thread {
+        HttpResponse<JsonNode> response_return;
+
         public void run() {
-            try{
-                HttpResponse<JsonNode> response = Unirest.post("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/products/classify")
-                        .header("X-Mashape-Key", "OkS1xENCS8mshJ1RJzjn5X9y1Ij5p11nRF2jsnMlSkI0S2WUan")
-                        .header("Content-Type", "application/json")
+            try {
+                HttpResponse<JsonNode> response = Unirest.get(URL)
+                        .header("X-Mashape-Key", KEY)
                         .header("Accept", "application/json")
-                        .body("{\"title\":\"Kroger Vitamin A & D Reduced Fat 2% Milk\",\"upc\":\"\",\"plu_code\":\"\"}")
                         .asJson();
-                System.out.println(response.getBody().getObject().toString());
-            }
-            catch (UnirestException e) {
+                setResponse(response);
+            } catch (UnirestException e) {
                 e.getStackTrace();
             }
-
         }
+
+        public void setResponse(HttpResponse<JsonNode> response) {
+            response_return = response;
+        }
+
+        public HttpResponse<JsonNode> getResponse() {
+            return response_return;
+        }
+
     }
 
     @Override
