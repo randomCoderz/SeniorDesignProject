f973b340fc14fc80a1861ca8559520551cbd829a
Am <am260700@gmail.com>
2018-02-21 11:35:54 -0800

    Room Persistence Update

--- app/src/main/java/com/pr/pr/pantryraid/roomPersist/DAO.java
+++ app/src/main/java/com/pr/pr/pantryraid/roomPersist/DAO.java
@@ -1,13 +1,18 @@
 package com.pr.pr.pantryraid.roomPersist;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
-
+@Dao
 public interface DAO {
     @Insert
-    public void testPass(recipes r1);
+    void testPass(recipes r1);
 
     @Query("SELECT * FROM recipes")
-    public recipes getRecipes();
+    recipes getRecipes();
+
+    @Delete
+    void delete(recipes recipe);
 
 }
--- app/src/main/java/com/pr/pr/pantryraid/roomPersist/recipes.java
+++ app/src/main/java/com/pr/pr/pantryraid/roomPersist/recipes.java
@@ -1,25 +1,51 @@
 package com.pr.pr.pantryraid.roomPersist;
 
 import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.PrimaryKey;
 
+@Entity
 public class recipes {
-        //Constructor for roomPersist
-        public recipes(String name, int recipeId, double price){
-            this.name = name;
-            this.recipeId = recipeId;
-            this.price = price;
-        }
+    @PrimaryKey
+    private int recipeId;
 
-        @PrimaryKey
-        private int recipeId;
+    @ColumnInfo
+    private double price;
 
-        @ColumnInfo
-        private double price;
+    //Foreign Key to be used from Ingredients
+    @ColumnInfo(name = "Name")
+    private String name;
 
-        //Foreign Key to be used from Ingredients
-        @ColumnInfo(name = "Name")
-        private String name;
+    //Constructor for roomPersist
+    public recipes(String name, int recipeId, double price){
+    this.name = name;
+    this.recipeId = recipeId;
+    this.price = price;
+    }
+    //Getters and Setters
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
 
