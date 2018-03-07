343e6a52fa49ebcd6b625d5279c3ff8bab90e246
Benjamin Guerrero <chemamin@gmail.com>
2018-03-07 09:42:42 -0800

    Navigation bar working, just need all activities to link them all.

---
+++ app/src/main/java/com/pr/pr/pantryraid/Settings.java
@@ -0,0 +1,21 @@
+package com.pr.pr.pantryraid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Settings extends Fragment {
+
+    public Settings(){
+
+    }
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.settings, container, false);
+        return rootView;
+    }
+}
