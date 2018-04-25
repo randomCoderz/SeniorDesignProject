e0eef4e0f27d5e0d387474737c5d13afac1609e3
Kan <kanyamamoto77@gmail.com>
2018-04-25 09:08:33 -0700

    fixed some recipe stuff

--- app/src/main/java/com/pr/pr/pantryraid/ingredient.java
+++ app/src/main/java/com/pr/pr/pantryraid/ingredient.java
@@ -9,6 +9,7 @@ public class ingredient
     String photoURL;
     int photoID;
     double quantity;
+    boolean missing;
     boolean selected;
 
 
@@ -37,13 +38,15 @@ public class ingredient
 
 
 
-    ingredient(int id, String name, String amount, String unit, String photoURL, double quantity, boolean selected)
+    ingredient(int id, String name, String amount, String unit, String photoURL, double quantity, boolean missing, boolean selected)
     {
         this.id = id;
         this.name = name;
         this.amount = amount;
         this.unit = unit;
         this.photoURL = photoURL;
+        this.quantity = quantity;
+        this.missing = missing;
         this.selected = selected;
     }
 
@@ -109,5 +112,13 @@ public class ingredient
 
     public void setQuantity(double quantity) {
         this.quantity = quantity;
+    }
+
+    public boolean isMissing() {
+        return missing;
+    }
+
+    public void setMissing(boolean missing) {
+        this.missing = missing;
     }
 }
