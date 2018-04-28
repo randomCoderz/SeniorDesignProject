c8def1853e98ff07c3589c0b1aa2eb06aad537df
Unknown <am260700@gmail.com>
2018-04-28 15:39:20 -0700

    Clean Master Part 2
    Master Fixed after many issues

---
+++ app/src/main/java/com/pr/pr/pantryraid/DietRestrictHolder.java
@@ -0,0 +1,9 @@
+package com.pr.pr.pantryraid;
+
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class DietRestrictHolder {
+    TextView restriction;
+    CheckBox selected;
+}
---
+++ app/src/main/java/com/pr/pr/pantryraid/DietRestrictName.java
@@ -0,0 +1,10 @@
+package com.pr.pr.pantryraid;
+
+public class DietRestrictName {
+    String name;
+    boolean selected;
+    DietRestrictName(String name){
+        this.name = name;
+    }
+
+}
